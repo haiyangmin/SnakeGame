f701d429fbec7112b2da4567cc8804b3f209fd48
Wangjun Yang <wangjun1yang@gmail.com>
2018-01-15 02:00:46 +0800

    Update Snakegame.js

--- Snakegame.js
+++ Snakegame.js
@@ -1,28 +1,48 @@
+var snake =[{x:1,y:1},{x:2,y:1},{x:3,y:1},{x:4,y:1}];
+var direction = "right";
+var snakeSize = 20;
+
 function drawBoard(){
  canv=document.getElementById("canvas");
     ctx=canv.getContext("2d");
     var w = 400;
     var h = 400;
-    var direction = "right";
     ctx.fillStyle = 'lightgrey';
     ctx.fillRect(0, 0, w, h);
     ctx.strokeStyle = 'black';
     ctx.strokeRect(0, 0, w, h);
 }
 
-function createSnake (){
- var snake =[{x:1,y:2},{x:2,y:2},{x:3,y:2},{x:4,y:2},];
- return snake;
- }
  
-function drawSnake (){
-   createSnake();
-    var snakeSize = 20;
-    var snake =[{x:1,y:2},{x:2,y:2},{x:3,y:2},{x:4,y:2},];
+function drawSnake (snake){
  for (let i = 0; i < 4; i++) {
         ctx.fillStyle = 'green';
         ctx.fillRect(snake[i].x*snakeSize, snake[i].y*snakeSize, snakeSize, snakeSize);
         ctx.strokeStyle = 'darkgreen';
         ctx.strokeRect(snake[i].x*snakeSize, snake[i].y*snakeSize, snakeSize, snakeSize);
-}
-}
+    }
+    }
+function drawFood(X,Y){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(X*snakeSize, Y*snakeSize, snakeSize, snakeSize);
+ 
+        ctx.strokeStyle = 'yellow';
+        ctx.strokeRect(X*snakeSize, Y*snakeSize, snakeSize, snakeSize);
+    }
+
+function generateFood (){
+          food = {
+            x: Math.floor((Math.random() * 20) ),
+            y: Math.floor((Math.random() * 20) )
+        }
+        for (var i=0; i<snake.length; i++) {
+            var snakeX = snake[i].x;
+            var snakeY = snake[i].y;     
+      if (food.x===snakeX  && food.y === snakeY) {
+          food.x = Math.floor((Math.random() * 20) - 1);
+          food.y = Math.floor((Math.random() * 20) - 1);
+            }
+        }
+    }
+
+
